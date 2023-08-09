2a57122f20863b8411d9fefb7309f5f4fa91ccff
Juan Cardenas <141847324+qkisito@users.noreply.github.com>
2023-08-09 13:47:48 -0500

    Uploading the Jupyter Notebook
    Python file that contains the final assignment.

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, will be present the Data Science Tools and Ecosystem learned in the course.
+
+# **Objectives:**
+# - List popular programming languages used in Data Science.
+# - List libraries used in Data Science.
+# - Name development environment open souce tools used in Data Science.
+# - Make arithmetic expression in Python.
+
+# In Data Science, you could use a lot of programming languages, such as:
+# 1) Python.
+# 2) R.
+# 3) Julia.
+# 4) Java.
+# 5) Stata.
+
+# Furthermore, there are useful library that could provide you multiple functions, like:
+# 1) NumPy.
+# 2) Matplotlib.
+# 3) Pandas.
+# 4) ScikitLearn.
+# 5) GGPlot.
+
+# |Data Science Tools|
+# |------------------|
+# |Anaconda|
+# |VSCode|
+# |RStudio|
+
+# ### On the other hand, these are some example of arithmetic expressions in Python:
+
+# In[1]:
+
+
+#Here is presented a simple arithmetic expression, where is multiplied and added integer numbers.
+(3*4)+5
+
+
+# In[3]:
+
+
+#This is an example that explain how to convert minutes to hours, just diving the number of minutes by 60.
+minutes = 200
+hours = minutes / 60
+print(hours)
+
+
+# ## Author
+# Juan David Cardenas Aguilar
